e8d4422e53ad89659cd94f8f9023baca11ca74ed
Joao Santanna <jsantanna@gmail.com>
2026-05-15 11:04:02 -0300

    atualizacao dia 15 de maio

--- Lista_de_exercicios/Q19.py
+++ Lista_de_exercicios/Q19.py
@@ -6,7 +6,7 @@ n = int(input('Informe o valor de n:'))
 
 # processamento
 contador = 0
-for i in range(1 , n + 1 ):
+for i in range(n , 0 , -1): # começa a processa em n e vai ate o 1
     if n % i == 0:
         print(f'divisor {i}')
         contador = contador + 1
